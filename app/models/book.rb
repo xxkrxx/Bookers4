2078514c2496975664aa1ef8c893ec4a9156c48d
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-11-27 17:39:59 +0900

    モデルコメントアウト

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,32 +1,52 @@
 class Book < ApplicationRecord
-has_one_attached :image
-belongs_to :user
-has_many :favorites, dependent: :destroy
-has_many :book_comments, dependent: :destroy
-has_many :week_favorites, -> {where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
-scope :latest, -> {order(created_at: :desc)}
-scope :old, -> {order(created_at: :asc)}
-scope :star_count, -> {order(star: :desc)}
-has_many :view_counts, dependent: :destroy
+  # Active Storageを使用してBookモデルに画像を添付します
+  has_one_attached :image
 
+  # Userモデルとの関連付けを表すbelongs_toアソシエーション
+  belongs_to :user
+
+  # Favoriteモデルとの関連付けを表すhas_manyアソシエーション。dependent: :destroyは関連づけられたお気に入りが削除される際にそれらも削除する
+  has_many :favorites, dependent: :destroy
+
+  # BookCommentモデルとの関連付けを表すhas_manyアソシエーション。dependent: :destroyは関連づけられたコメントが削除される際にそれらも削除する
+  has_many :book_comments, dependent: :destroy
+
+  # ViewCountモデルとの関連付けを表すhas_manyアソシエーション。dependent: :destroyは関連づけられたビューカウントが削除される際にそれらも削除する
+  has_many :view_counts, dependent: :destroy
+
+  # 直近1週間以内に作成されたお気に入りを取得するためのスコープを定義
+  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
+
+  # 作成日時の降順でソートするスコープを定義
+  scope :latest, -> { order(created_at: :desc) }
+
+  # 作成日時の昇順でソートするスコープを定義
+  scope :old, -> { order(created_at: :asc) }
+
+  # 星の数で降順にソートするスコープを定義
+  scope :star_count, -> { order(star: :desc) }
+
+  # タイトル、本文、カテゴリが存在するかを検証するバリデーションを定義
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
   validates :category, presence: true
 
-
+  # 特定のユーザーがこの本をお気に入りにしているかどうかを確認するメソッドを定義
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
+  # コンテンツと検索方法に基づいて本を検索するためのメソッドを定義
   def self.search_for(content, method)
-   if method == "perfect"
-    Book.where(name: content)
-   elsif method == 'forward'
-    Book.where('name LIKE ?', content + '%')
-   elsif method == 'backward'
-    Book.where('name LIKE ?', '%' + content)
-   else
-    Book.where('name LIKE ?', '%' + content + '%') 
-   end
+    if method == "perfect"
+      Book.where(name: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,63 +1,75 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseモジュールを使用してユーザー認証機能を追加します
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   has_many :books
-   has_many :favorites, dependent: :destroy
-   has_many :book_comments, dependent: :destroy
+  # このユーザーが投稿した本に対する関連付けを表すhas_manyアソシエーション
+  has_many :books
 
+  # お気に入りに関する関連付けを表すhas_manyアソシエーション
+  has_many :favorites, dependent: :destroy
 
-   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-   has_many :followers, through: :reverse_of_relationships, source: :follower
-   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-   has_many :followings, through: :relationships, source: :followed
-   has_many :send_user, dependent: :destroy
-   has_many :receive_user, dependent: :destroy
-   has_many :view_counts, dependent: :destroy 
+  # 本に対するコメントに関する関連付けを表すhas_manyアソシエーション
+  has_many :book_comments, dependent: :destroy
 
+  # フォロワーとの関連付けを表すhas_manyアソシエーション
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  # フォローしているユーザーとの関連付けを表すhas_manyアソシエーション
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
 
+  # メッセージの送信者と受信者に関する関連付けを表すhas_manyアソシエーション
+  has_many :send_user, dependent: :destroy
+  has_many :receive_user, dependent: :destroy
+
+  # ビューカウントに関する関連付けを表すhas_manyアソシエーション
+  has_many :view_counts, dependent: :destroy
+
+  # プロフィール画像を添付するためのActive Storageの関連付け
   has_one_attached :profile_image
 
-
+  # バリデーション：ユーザー名が存在し、一意であること、長さは2から20文字の範囲内であること
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+
+  # バリデーション：自己紹介文の長さが50文字以内であること
   validates :introduction, length: { maximum: 50 }
 
-
- def get_profile_image(width, height)
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  # プロフィール画像がない場合はデフォルトの画像を表示するメソッド
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  profile_image.variant(resize_to_limit: [width, height]).processed
- end
 
- def follow(user)
-   relationships.create(followed_id: user.id)
- end
+  # ユーザーをフォローするメソッド
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
 
- def unfollow(user)
-   relationships.find_by(followed_id: user.id).destroy
- end
+  # ユーザーのフォローを解除するメソッド
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
 
- def following?(user)
-   followings.include?(user)
- end
+  # 特定のユーザーをフォローしているかどうかを確認するメソッド
+  def following?(user)
+    followings.include?(user)
+  end
 
-
-
- def self.search_for(content, method)
-   if method == "perfect"
-    User.where(name: content)
-   elsif method == 'forward'
-    User.where('name LIKE ?', content + '%')
-   elsif method == 'backward'
-    User.where('name LIKE ?', '%' + content)
-   else
-    User.where('name LIKE ?', '%' + content + '%') 
-   end
- end
- 
+  # ユーザーを検索するメソッド
+  def self.search_for(content, method)
+    if method == "perfect"
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
 end
