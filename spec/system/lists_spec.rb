b2b66171f0c88e8bc451988cb15e32983da28741
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2024-01-16 02:21:22 +0000

    テストコード追加

---
+++ spec/system/lists_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe '投稿のテスト' do
+  let!(:list) { creste(:list, title: 'hoge',body: 'body') }
+   describe：'トップ画面(top_path)のテスト' do
+     before do 
+       visit_path
+      end
+      
+end  
