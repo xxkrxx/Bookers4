2e7e61d7cf946a5a6a422a63de9dcf47ee8afa7d
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2024-01-17 01:01:08 +0000

    一応

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -1,10 +1,76 @@
 require 'rails_helper'
 
 describe '投稿のテスト' do
-  let!(:list) { creste(:list, title: 'hoge',body: 'body') }
-   describe：'トップ画面(top_path)のテスト' do
-     before do 
-       visit_path
+  let!(:list) { create(:list, title: 'hoge', body: 'body') }
+
+  describe 'トップ画面(top_path)のテスト' do
+    before do 
+      visit top_path
+    end
+
+    context '表示の確認' do
+      it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
+        expect(page).to have_content 'ここはTopページです'
       end
-      
-end  
+
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/top')
+      end
+    end
+  end
+  
+  describe "投稿画面(new_list_path)のテスト"
+  before do 
+    visit new_list_path
+    end
+    context '表示の確認' do
+        it 'new_list_pathが"/lists/new"であるか' do
+          expect(current_path).to eq('/list/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_button '投稿'
+        expect(page).to have_current_path list_path(List.last)
+      end
+    end
+  end
+  
+    describe "投稿一覧のテスト" do
+      before do
+        visit lists_path
+      end
+    context '表示の確認' do
+      it '投稿されたものが表示されているか' do
+        expect(page).to have_content list.title
+        expect(page).to have_link list.title
+      end
+    end
+  end  
+
+    describe "詳細画面のテスト" do
+      before do
+        visit list_path(list)
+      end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        expect(page).to have_link '編集'
+      end
+    end 
+    context 'リンクの遷移先の確認' do
+      it '編集の遷移先は編集画面か' do
+        edit_link = find_all('a') [3] # ?
+        edit_link.click
+        expect(current_path).to eq('/list/' + list.id.to_s + '/edit')
+      end
+    end
+  end
+
